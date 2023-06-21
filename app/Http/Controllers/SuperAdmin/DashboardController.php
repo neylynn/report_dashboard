73e52684d808d4dd2f8e29bcfe703e87e59b282d
neylynn <neylynn97@gmail.com>
2023-06-21 16:16:47 +0630

    fix: change code for  portal 2

--- app/Http/Controllers/SuperAdmin/DashboardController.php
+++ app/Http/Controllers/SuperAdmin/DashboardController.php
@@ -149,135 +149,137 @@ class DashboardController extends Controller
             case 'portal_two':
 
                 // old code
-                $db = \DB::connection('mysql_portal_two');
-                $database = $db->getDatabaseName();
-                $host = config('ssh.portal_two_host');
-                $username = config('ssh.username');
-                $privateKeyPath = config('ssh.pem_key');
-                $mysql_username = config('ssh.portal_two_username');
+                // $db = \DB::connection('mysql_portal_two');
+                // $database = $db->getDatabaseName();
+                // $host = config('ssh.portal_two_host');
+                // $username = config('ssh.username');
+                // $privateKeyPath = config('ssh.pem_key');
+                // $mysql_username = config('ssh.portal_two_username');
 
-                $privateKey = PublicKeyLoader::load($privateKeyPath);$db = \DB::connection('mysql_portal_two');
-                $database = $db->getDatabaseName();
-                $host = config('ssh.portal_two_host');
-                $username = config('ssh.username');
-                $privateKeyPath = config('ssh.pem_key');
-                $mysql_username = config('ssh.portal_two_username');
+                // $privateKey = PublicKeyLoader::load($privateKeyPath);$db = \DB::connection('mysql_portal_two');
+                // $database = $db->getDatabaseName();
+                // $host = config('ssh.portal_two_host');
+                // $username = config('ssh.username');
+                // $privateKeyPath = config('ssh.pem_key');
+                // $mysql_username = config('ssh.portal_two_username');
 
-                $privateKey = PublicKeyLoader::load($privateKeyPath);
-                $ssh = new SSH2($host);
+                // $privateKey = PublicKeyLoader::load($privateKeyPath);
+                // $ssh = new SSH2($host);
 
-                if (!$ssh->login($username, $privateKey)) {
-                    exit('Login Failed');
-                }
+                // if (!$ssh->login($username, $privateKey)) {
+                //     exit('Login Failed');
+                // }
 
-                $start_date = $request->input('start_date');
-                $end_date = $request->input('end_date');
-                $company_list_command = 'MYSQL_PWD="BpbS8Tc38.s/-p>E+" mysql -u ' . $mysql_username . ' -e "USE ' . $database . '; CALL company_list()"';
-                $company_result = $ssh->exec($company_list_command);
-                $cleanedList = str_replace("id_list", "", trim($company_result));
-                $array = explode(",", $cleanedList);
-                $cleaned_array = array_map('trim', $array);
-                $company_array = array_map('intval', $cleaned_array);
-                $result_array = [];
+                // $start_date = $request->input('start_date');
+                // $end_date = $request->input('end_date');
+                // $company_list_command = 'MYSQL_PWD="BpbS8Tc38.s/-p>E+" mysql -u ' . $mysql_username . ' -e "USE ' . $database . '; CALL company_list()"';
+                // $company_result = $ssh->exec($company_list_command);
+                // $cleanedList = str_replace("id_list", "", trim($company_result));
+                // $array = explode(",", $cleanedList);
+                // $cleaned_array = array_map('trim', $array);
+                // $company_array = array_map('intval', $cleaned_array);
+                // $result_array = [];
 
-                foreach ($company_array as $companyId) {
-                    $get_data = 'MYSQL_PWD="BpbS8Tc38.s/-p>E+" mysql -u ' . $mysql_username . ' -e "USE ' . $database . '; CALL viber_report(\'' . $start_date . '\', \'' . $end_date . '\',\'' . $companyId . '\')"';
-                    $datas = $ssh->exec($get_data);
-                    Log::debug('Received data: ' . $datas); // Log the received data
+                // foreach ($company_array as $companyId) {
+                //     $get_data = 'MYSQL_PWD="BpbS8Tc38.s/-p>E+" mysql -u ' . $mysql_username . ' -e "USE ' . $database . '; CALL viber_report(\'' . $start_date . '\', \'' . $end_date . '\',\'' . $companyId . '\')"';
+                //     $datas = $ssh->exec($get_data);
+                //     Log::debug('Received data: ' . $datas); // Log the received data
 
-                    $array = explode("\t", $datas);
+                //     $array = explode("\t", $datas);
 
-                    // Check if $array has enough elements
-                    if (count($array) >= 7) {
-                        $string = $array[3];
-                        $parenthesisPosition = strpos($string, ')');
-                        $trimmedString = trim(substr($string, $parenthesisPosition + 1));
-                        $data = [
-                            [
-                                $trimmedString,
-                                $array[4],
-                                $array[5],
-                                $array[6]
-                            ]
-                        ];
-                        array_push($result_array, $data);
-                    } else {
-                        // Log the error
-                        Log::error('Error: Insufficient data elements in $array on companyId ' . $companyId);
-                        // You can also include additional information in the log message if needed
+                //     // Check if $array has enough elements
+                //     if (count($array) >= 7) {
+                //         $string = $array[3];
+                //         $parenthesisPosition = strpos($string, ')');
+                //         $trimmedString = trim(substr($string, $parenthesisPosition + 1));
+                //         $data = [
+                //             [
+                //                 $trimmedString,
+                //                 $array[4],
+                //                 $array[5],
+                //                 $array[6]
+                //             ]
+                //         ];
+                //         array_push($result_array, $data);
+                //     } else {
+                //         // Log the error
+                //         Log::error('Error: Insufficient data elements in $array on companyId ' . $companyId);
+                //         // You can also include additional information in the log message if needed
 
-                        // Or you can throw an exception
-                        // throw new Exception('Error: Insufficient data elements in $array on companyId ' . $companyId);
-                    }
-                }
-
-                return Excel::download(new \App\Exports\ProcedureDataExport(json_decode(json_encode($result_array), true)), 'portal_two_viber_report(' . $start_date . '_' . $end_date . ').xlsx');
+                //         // Or you can throw an exception
+                //         // throw new Exception('Error: Insufficient data elements in $array on companyId ' . $companyId);
+                //     }
+                // }
+                
+                // return Excel::download(new \App\Exports\ProcedureDataExport(json_decode(json_encode($result_array), true)), 'portal_two_viber_report(' . $start_date . '_' . $end_date . ').xlsx');
 
 
                 //new code
 
-                // $start_date = $request->input('start_date');
-                // $end_date = $request->input('end_date');
-                // $company_array = [];
+                $start_date = $request->input('start_date');
+                $end_date = $request->input('end_date');
+                $company_array = [];
 
-                // // Retrieve company IDs using company_list procedure
-                // try {
-                //     $host = env('DB_PORTAL_TWO_HOST');
-                //     $database = env('DB_PORTAL_TWO_DATABASE');
-                //     $username = env('DB_PORTAL_TWO_USERNAME');
-                //     $password = env('DB_PORTAL_TWO_PASSWORD');
+                // Retrieve company IDs using company_list procedure
+                try {
+                    $host = env('DB_PORTAL_TWO_HOST');
+                    $database = env('DB_PORTAL_TWO_DATABASE');
+                    $username = env('DB_PORTAL_TWO_USERNAME');
+                    $password = env('DB_PORTAL_TWO_PASSWORD');
 
-                //     $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $username, $password);
+                    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $username, $password);
 
-                //     // Execute the company_list procedure
-                //     $stmt = $pdo->prepare('CALL company_list()');
-                //     $stmt->execute();
-                //     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                    // Execute the company_list procedure
+                    $stmt = $pdo->prepare('CALL company_list()');
+                    $stmt->execute();
+                    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                //     foreach ($result as $row) {
-                //         $company_id = $row['id'];
-                //         array_push($company_array, $company_id);
-                //     }
-                // } catch (PDOException $e) {
-                //     // Log or handle the database connection error
-                //     Log::error('Database Connection Error: ' . $e->getMessage());
-                //     // Return an error response to the user
-                //     return response()->json(['error' => 'Failed to connect to the database'], 500);
-                // }
+                    foreach ($result as $row) {
+                        $company_id = $row['id'];
+                        array_push($company_array, $company_id);
+                    }
+                } catch (PDOException $e) {
+                    // Log or handle the database connection error
+                    Log::error('Database Connection Error: ' . $e->getMessage());
+                    // Return an error response to the user
+                    return response()->json(['error' => 'Failed to connect to the database'], 500);
+                }
                 // dd($company_array);
 
-                // $result_array = [];
-                // foreach ($company_array as $companyId) {
-                //     try {
-                //         $stmt = $pdo->prepare('CALL viber_report(?, ?, ?)');
-                //         $stmt->bindParam(1, $start_date, PDO::PARAM_STR);
-                //         $stmt->bindParam(2, $end_date, PDO::PARAM_STR);
-                //         $stmt->bindParam(3, $companyId, PDO::PARAM_INT);
-                //         $stmt->execute();
-                //         $arrayOfObjects = $stmt->fetchAll(PDO::FETCH_OBJ);
+                $result_array = [];
+                foreach ($company_array as $companyId) {
+                    try {
+                        $stmt = $pdo->prepare('CALL viber_report(?, ?, ?)');
+                        $stmt->bindParam(1, $start_date, PDO::PARAM_STR);
+                        $stmt->bindParam(2, $end_date, PDO::PARAM_STR);
+                        $stmt->bindParam(3, $companyId, PDO::PARAM_INT);
+                        $stmt->execute();
+                        $arrayOfObjects = $stmt->fetchAll(PDO::FETCH_OBJ);
 
-                //         foreach ($arrayOfObjects as $object) {
-                //             $company_name = $object->name;
-                //             $delivered = $object->Delivered;
-                //             $seen = $object->Seen;
-                //             $error = $object->Error;
-                //             $data = [
-                //                 $company_name,
-                //                 $delivered,
-                //                 $seen,
-                //                 $error
-                //             ];
-                //             array_push($result_array, $data);
-                //         }
-                //     } catch (PDOException $e) {
-                //         // Log or handle the database connection error
-                //         Log::error('Database Connection Error: ' . $e->getMessage());
-                //         // Return an error response to the user
-                //         return response()->json(['error' => 'Failed to connect to the database'], 500);
-                //     }
-                // }
+                        foreach ($arrayOfObjects as $object) {
+                            $company_name = $object->name;
+                            $delivered = $object->Delivered;
+                            $seen = $object->Seen;
+                            $error = $object->Error;
+                            $data = [
+                                $company_name,
+                                $delivered,
+                                $seen,
+                                $error
+                            ];
+                            array_push($result_array, $data);
+                        }
+                    } catch (PDOException $e) {
+                        // Log or handle the database connection error
+                        Log::error('Database Connection Error: ' . $e->getMessage());
+                        // Return an error response to the user
+                        return response()->json(['error' => 'Failed to connect to the database'], 500);
+                    }
+                }
 
-                // return Excel::download(new \App\Exports\ProcedureDataExport(json_decode(json_encode($result_array), true)), 'portal_two_viber_report(' . $start_date . '_' . $end_date . ').xlsx');
+                
+                Log::debug(json_decode(json_encode($result_array), true));
+                return Excel::download(new \App\Exports\ProcedureDataExport(json_decode(json_encode($result_array), true)), 'portal_two_viber_report(' . $start_date . '_' . $end_date . ').xlsx');
 
                 break;
             case 'roche':
